0fcf1c089f9d4bb10af265d83a77e807a737cf57
Jonathan Lehman <jonathan.lehman91@gmail.com>
2018-09-27 21:02:53 -0400

    Add tests

--- cache.go
+++ cache.go
@@ -1,15 +1,20 @@
 package cache
 
 import (
+	"errors"
 	"time"
 
 	"github.com/dgraph-io/badger"
 )
 
+// LocalCache is an interface implementing CacheMiss that is called
+// to hydrate the cache when fetching data via `Fetch` results in a miss
 type LocalCache interface {
 	CacheMiss(key string) ([]byte, error)
 }
 
+// BadgerCache wraps an instance of a badger database with automatic garbage collection
+// a TTL of 0 does not expire keys
 type BadgerCache struct {
 	name   string
 	db     *badger.DB
@@ -17,17 +22,23 @@ type BadgerCache struct {
 	ticker *time.Ticker // for GC loop
 }
 
-func NewCache(n string, t time.Duration) (*BadgerCache, error) {
-	opts := badger.DefaultOptions
+// NewCache creates a new BadgerCache
+func NewCache(n string, t time.Duration, opts *badger.Options) (*BadgerCache, error) {
+	if t < time.Second && t > 0 {
+		return &BadgerCache{}, errors.New("TTL must be >= 1 second. Badger uses Unix timestamps for expiries which operate in second resolution")
+	}
+	if opts == nil {
+		opts = &badger.DefaultOptions
+	}
 	opts.Dir = n
 	opts.ValueDir = n
-	db, err := badger.Open(opts)
+	db, err := badger.Open(*opts)
 	if err != nil {
 		return &BadgerCache{}, err
 	}
 
 	// start a GC loop
-	ticker := time.NewTicker(30 * time.Minute)
+	ticker := time.NewTicker(5 * time.Minute)
 	go func(t *time.Ticker, d *badger.DB) {
 		for range t.C {
 		again:
@@ -42,15 +53,21 @@ func NewCache(n string, t time.Duration) (*BadgerCache, error) {
 	return &c, nil
 }
 
+// Close closes the badger database
 func (c *BadgerCache) Close() error {
 	c.ticker.Stop()
 	return c.db.Close()
 }
 
+// Fetch gets data from the cache for the specified key
+// If the data is missing, the result from LocalCache.CacheMiss is returned and stored to the key
+// uses the LocalCache TTL by default
 func (c *BadgerCache) Fetch(k []byte, l LocalCache) ([]byte, error) {
 	return c.FetchWithTTL(k, l, c.TTL)
 }
 
+// FetchWithTTL is the same as Fetch, but with an explicit TTL
+// a TTL of 0 does not expire keys
 func (c *BadgerCache) FetchWithTTL(k []byte, l LocalCache, ttl time.Duration) ([]byte, error) {
 	var ret []byte
 	cacheMiss := false
@@ -73,27 +90,35 @@ func (c *BadgerCache) FetchWithTTL(k []byte, l LocalCache, ttl time.Duration) ([
 			return ret, err
 		}
 
-		go c.db.Update(func(txn *badger.Txn) error {
-			return setWithTTL(txn, k, ret, ttl)
+		c.db.Update(func(txn *badger.Txn) error {
+			err := setWithTTL(txn, k, ret, ttl)
+			return err
 		})
 	}
 	return ret, err
 }
 
+// Set writes data to the cache with the default cache TTL
 func (c *BadgerCache) Set(k, v []byte) error {
 	return c.SetWithTTL(k, v, c.TTL)
 }
 
+// SetWithTTL writes data to the cache with an explicit TTL
+// a TTL of 0 does not expire keys
 func (c *BadgerCache) SetWithTTL(k, v []byte, ttl time.Duration) error {
 	return c.db.Update(func(txn *badger.Txn) error {
 		return setWithTTL(txn, k, v, ttl)
 	})
 }
 
+// SetBatch writes data to the cache with the default cache TTL
+// this is more performant than calling `Set` in a loop if data to store is already known
 func (c *BadgerCache) SetBatch(k, v [][]byte) error {
 	return c.SetBatchWithTTL(k, v, c.TTL)
 }
 
+// SetBatchWithTTL writes data to the cache with an explicit TTL
+// a TTL of 0 does not expire keys
 func (c *BadgerCache) SetBatchWithTTL(k, v [][]byte, ttl time.Duration) error {
 	return c.db.Update(func(txn *badger.Txn) error {
 		for i := range k {
@@ -103,32 +128,34 @@ func (c *BadgerCache) SetBatchWithTTL(k, v [][]byte, ttl time.Duration) error {
 	})
 }
 
+// Incr increments the key by the specified uint64 value and returns the current value
+// due to transaction conflicts this is eventually consistent
 func (c *BadgerCache) Incr(k []byte, v uint64) (uint64, error) {
-	return c.IncrWithTTL(k, v, c.TTL)
-}
-
-func (c *BadgerCache) IncrWithTTL(k []byte, v uint64, ttl time.Duration) (uint64, error) {
-	var ret uint64
-	err := c.db.Update(func(txn *badger.Txn) error {
-		item, err := txn.Get(k)
-		if err == badger.ErrKeyNotFound {
-			ret = v
-		} else if err != nil {
-			return err
-		} else {
-			val, err := item.Value()
-			if err != nil {
-				return err
-			}
-			ret = bytesToUint64(val) + v
+	b, err := c.Get(k)
+	if err == badger.ErrKeyNotFound {
+		err = c.SetWithTTL(k, uint64ToBytes(v), 0)
+		if err != nil {
+			return 0, err
 		}
+		return v, nil
+	} else if err != nil {
+		return bytesToUint64(b), err
+	}
 
-		return setWithTTL(txn, k, uint64ToBytes(ret), ttl)
-	})
-
-	return ret, err
+	m := c.db.GetMergeOperator(k, add, 50*time.Millisecond)
+	defer m.Stop()
+	err = m.Add(uint64ToBytes(v))
+	if err != nil {
+		return 0, err
+	}
+	b, err = m.Get()
+	if err != nil {
+		return 0, err
+	}
+	return bytesToUint64(b), nil
 }
 
+// Get retrieves data for a key from the cache
 func (c *BadgerCache) Get(k []byte) ([]byte, error) {
 	ret := []byte{}
 	err := c.db.View(func(txn *badger.Txn) error {
@@ -144,7 +171,9 @@ func (c *BadgerCache) Get(k []byte) ([]byte, error) {
 
 func setWithTTL(txn *badger.Txn, k, v []byte, ttl time.Duration) error {
 	// set the new value with TTL
-	if ttl > 0 {
+	if ttl < time.Second && ttl > 0 {
+		return errors.New("TTL must be >= 1 second. Badger uses Unix timestamps for expiries which operate in second resolution")
+	} else if ttl > 0 {
 		err := txn.SetWithTTL(k, v, ttl)
 		if err != nil {
 			return err
