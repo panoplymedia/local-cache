a30c536313df8901b76779cd7a31ea5756eed13d
Brian Freeman <brian.freeman@slate.com>
2018-10-07 19:49:42 -0400

    add sharding

--- memory_cache.go
+++ memory_cache.go
@@ -1,13 +1,14 @@
 package cache
 
 import (
+	"strings"
 	"sync"
 	"time"
 )
 
 type MemoryCache struct {
-	Dat      map[string]cacheElement
-	mu       sync.RWMutex
+	Dat      [26]map[string]cacheElement
+	mu       [26]sync.RWMutex
 	KeyCount uint64
 }
 
@@ -17,31 +18,36 @@ type cacheElement struct {
 }
 
 func newMemoryCache() *MemoryCache {
-	d := map[string]cacheElement{}
-
 	var m MemoryCache
-	m.Dat = d
+	for i := 0; i < 26; i++ {
+		d := map[string]cacheElement{}
+		m.Dat[i] = d
+	}
 	return &m
 }
 
 func (m *MemoryCache) Read(key string) ([]byte, bool) {
-	m.mu.RLock()
-	el, exists := m.Dat[key]
-	m.mu.RUnlock()
+	idx := keyToShard(key)
+
+	m.mu[idx].RLock()
+	el, exists := m.Dat[idx][key]
+	m.mu[idx].RUnlock()
 	if exists && time.Now().UTC().Before(el.expiresAt) {
 		return el.dat, true
 	} else if exists {
 		// evict key since it exists and it's expired
-		m.mu.Lock()
-		delete(m.Dat, key)
+		m.mu[idx].Lock()
+		delete(m.Dat[idx], key)
 		m.KeyCount--
-		m.mu.Unlock()
+		m.mu[idx].Unlock()
 	}
 	return []byte{}, false
 }
 
 func (m *MemoryCache) Write(key string, val []byte, ttl time.Duration) {
+	idx := keyToShard(key)
 	var e time.Time
+
 	if ttl == 0 {
 		// for a 0 TTL, store the max value of a time struct, so it essentially never expires
 		e = time.Unix(1<<63-1, 0)
@@ -54,8 +60,19 @@ func (m *MemoryCache) Write(key string, val []byte, ttl time.Duration) {
 		dat:       val,
 	}
 
-	m.mu.Lock()
-	m.Dat[key] = c
+	m.mu[idx].Lock()
+	m.Dat[idx][key] = c
 	m.KeyCount++
-	m.mu.Unlock()
+	m.mu[idx].Unlock()
+}
+
+func keyToShard(key string) int {
+	i := int(strings.ToLower(key)[0])
+
+	// if we're not in the char range (97-122), we'll stick it in the z bucket
+	if i < 97 || i > 122 {
+		i = 122
+	}
+
+	return i - 97
 }
